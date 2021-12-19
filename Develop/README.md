da867269b500a4cb732ec7e52153cb62740d3a1e
Optical23 <40125489+Optical23@users.noreply.github.com>
2021-12-18 19:14:09 -0600

    Set up basic flow of inquirer for title and description

---
+++ Develop/README.md
@@ -0,0 +1,4 @@
+# aaron
+  ## Description 
+  project by aaron
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
-
+var inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
+// const utils = require('utils');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    { 
+        type: 'input',
+        name: 'description',
+        message: 'What is a description of your project?',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, markdown(data), err => {
+        (err) ? console.log(err) : console.log('File written: Success');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => { 
+            writeToFile('README.md', answers);
+        })
+        .catch((error) => {
+            (error) ? console.log(error) : console.log('No errors, Writing file...');
+        });
+}
 
 // Function call to initialize app
 init();
